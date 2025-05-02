98fa6ed8b5dfc4675966a1870995ff7f13c3d059
ThomasFzr <thfltzr@gmail.com>
2025-05-02 16:52:34 +0200

    Update global styles and add Navbar component

---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,9 @@
+export const Navbar = () => {
+    return(
+        <div className="w-full flex flex-row justify-between items-center p-4">
+            <div>Mes recettes</div>
+            <div>Explorer des recettes</div>
+            <div>Mon panier</div>
+        </div>
+    );
+};
