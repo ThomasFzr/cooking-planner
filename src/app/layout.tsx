772e0101202ceed064098b3945bcbd84520cc01d
ThomasFzr <thfltzr@gmail.com>
2025-05-08 15:03:04 +0200

    fix: update global styles and font configuration for consistency

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/Navbar/Navbar";
 import { AuthSessionProvider } from "@/components/AuthSession/AuthSession";
-// import { Anton } from "next/font/google";
+import { Anton } from "next/font/google";
 
-// const anton = Anton({
-//   variable: "--font-anton",
-//   subsets: ["latin"],
-// });
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const anton = Anton({
+  variable: "--font-anton",
   subsets: ["latin"],
+  weight: "400",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -34,10 +26,9 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet" />
       </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${anton.variable} `}
       >
         <AuthSessionProvider>
           <Navbar />
